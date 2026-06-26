c7abc3d3e1c8c186b3b9be72f7ea1493687b0665
lico <549063037@qq.com>
2026-06-26 17:00:47 +0800

    add

---
+++ src/settings.306a3.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/hello.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",main:"abc4b"}};
